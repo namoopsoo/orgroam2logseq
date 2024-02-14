e5d62773c82a92202f44b55446b5fe43221c8dbe
namoopsoo <namoopsoo@users.noreply.github.com>
2024-02-14 09:33:42 -0500

    Id title map

--- hello.go
+++ hello.go
@@ -8,7 +8,14 @@ import (
     "regexp"
 )
 
-func Migrate(sourceDir string, destinationDir string) error {
+func BuildIdTitleMap() {
+    var idMap map[string]string
+    // for each file
+    // FindIdTitle 
+}
+
+// find id, title for org file
+func FindIdTitle(sourceDir string, destinationDir string) error {
     fmt.Print(sourceDir, "->", destinationDir)
 
     var lines []string
@@ -21,16 +28,36 @@ func Migrate(sourceDir string, destinationDir string) error {
 
     // ec22c32c-26b5-45a7-992-ff867494e7
     idRegexp := "(:ID:) ([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{3}-[a-f0-9]{10})$"
-    re := regexp.MustCompile(idRegexp)
+    idRe := regexp.MustCompile(idRegexp)
+
+    titleRegexp := "#+:title (.*)$"
+    titleRe := regexp.MustCompile(titleRegexp)
+    
+    foundID := ""
+    foundTitle := ""
 
     for i, line := range lines {
-        matches := re.FindStringSubmatch(line)
+        matches := idRe.FindStringSubmatch(line)
         if len(matches) > 0 {
             fmt.Printf("%d, %v, match?\n", i, line)
             for j, m := range matches {
                 fmt.Println(j, m)
             }
+            foundID = matches[1]
         }
+
+        // also match title        
+        matches := titleRe.FindStringSubmatch(line)
+        if len(matches) > 0 {
+            fmt.Printf("%d, %v, match?\n", i, line)
+            for j, m := range matches {
+                fmt.Println(j, m)
+            }
+            foundTitle = matches[1]
+        }
+
+        // if find both id and title, break early
+        if foundID != "" and foundTitle != "" {break}
     }
 
     return nil
