a1680b3d5fb76341d44d4c4e79ed21b43d05ced0
namoopsoo <namoopsoo@users.noreply.github.com>
2024-03-04 09:15:11 -0500

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -1,7 +1,15 @@
 package hello_test
 
+import (
+    "fmt"
+    
+    "github.com/namoopsoo/orgroam2logseq/hello"
+    "testing"
+
+)
 import "testing"
 
 func TestFoo(t *testing.T) {
+    fmt.Println("hi")
     t.Errorf("uhoh")
 }
