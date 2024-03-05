e203a77b26908ab782f1359096530325b4765576
namoopsoo <namoopsoo@users.noreply.github.com>
2024-03-05 08:47:46 -0500

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -3,16 +3,16 @@ package main
 import (
     "fmt"
     
-    "github.com/namoopsoo/orgroam2logseq"
+    //"github.com/namoopsoo/orgroam2logseq"
     "testing"
 
 )
-import "testing"
+
 
 func TestFoo(t *testing.T) {
     fmt.Println("hi")
 
-    actual := hello.MakeNewFileName("yo/cool")
+    actual := main.MakeNewFileName("yo/cool")
     if actual != "yo___cool.md" {
         t.Errorf("uhoh, %v, ", actual)
     }
